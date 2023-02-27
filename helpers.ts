ecb93e53b2143f72a0481a3fec9a576f007e0662
Miroslaw Shpak <bytesbay@icloud.com>
2023-02-27 19:34:15 +0200

    save

--- helpers.ts
+++ helpers.ts
@@ -13,14 +13,14 @@ export function getDb() {
   return db
 }
 
-export const REPO_URL = 'https://github.com/bytesbay/articles';
+export const REPO_RAW_URL = 'https://raw.githubusercontent.com/bytesbay/articles/main';
 
 export async function parseMd(path: string) {
   const md_metadata: IArticleMetadata = parse(FS.readFileSync(path, 'utf8').split('---')[1]);
   const md_content: string = FS.readFileSync(path, 'utf8')
     .replace(/---((?!---).|\n|\r\n)*---/, '')
     .replace(/^\n/, '') // remove first newline
-    .replace(/!\[.*\]\((.*)\)/g, `![image](${REPO_URL}$1)`);
+    .replace(/!\[.*\]\((.*)\)/g, `![image](${REPO_RAW_URL}$1)`);
 
   return { md_metadata, md_content }
 }
